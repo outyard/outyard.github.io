9e13f0428176a10e6a2d38ad449d9f25d244d8d5
Carl Enlund <carl.enlund@gmail.com>
2020-01-26 16:39:16 +0100

    Tweak spacing

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,6 @@ const Footer = () => (
   <footer className="footer">
     <Container>
       <p><a href="mailto:carl.enlund@gmail.com">Contact</a></p>
-      <br/>
       <p><a href="https://github.com/outyard" target="_blank">Outyard on GitHub &#x2197;</a></p>
     </Container>
   </footer>
