e078752328332440b938888c3b51d9eb1b01e290
Carl Enlund <carl.enlund@gmail.com>
2020-03-02 10:35:37 +0100

    Update title

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ const IndexPage = () => (
 
     <Hackathon
         date="2020-02-28"
-        title="Make a game with character customization"
+        title="Make a game with a customizable character"
         start="17:00"
         end="00:00"
         total="7"
