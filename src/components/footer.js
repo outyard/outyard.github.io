4105eebe996b7e96e7940b320c7f4b756cc6dcbb
Carl Enlund <carl.enlund@gmail.com>
2020-03-03 09:17:13 +0100

    Tweaks

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,9 +8,13 @@ const Footer = () => (
   <footer className="footer">
     <Container>
       <img className="footer-logo" src="/images/logo-yellow.svg" alt="" />
+      <div class="footer-text">
+        <p>Outyard is an independently organized hackathon at Uppsala University. It is meant to inspire personal programming projects and is a great way to learn and work under intense deadlines.</p>
+      </div>
       <div className="footer-links">
         <p><a href="mailto:carl.enlund@gmail.com">Contact</a></p>
-        <p><a href="https://github.com/outyard" target="_blank">Outyard on GitHub &#x2197;</a></p>
+        <p><a href="https://facebook.com/outyardhackathon" target="_blank">Facebook &#x2197;</a></p>
+        <p><a href="https://github.com/outyard" target="_blank">GitHub &#x2197;</a></p>
       </div>
     </Container>
   </footer>
