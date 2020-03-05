98349d18b0a90a19f12af046ac8ffccb3549469e
Carl Enlund <carl.enlund@gmail.com>
2020-03-05 20:11:21 +0100

    Use proper internal link

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'gatsby';
 
 import Container from './container';
 
@@ -7,7 +8,7 @@ import './header.scss';
 const Header = () => (
   <header className="header">
     <Container>
-      <a className="header-logo-link" href="/"><img className="header-logo" src="/images/logo-blue.svg" alt="Outyard" /></a>
+      <Link className="header-logo-link" to="/"><img className="header-logo" src="/images/logo-blue.svg" alt="Outyard" /></Link>
       <div className="header-nav">
         <p className="header-nav-item">Hackathon @ Uppsala University</p>
       </div>
