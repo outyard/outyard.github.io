0b5921bacd4d348ad62fcd08441e4bd61b655d53
Carl Enlund <carl.enlund@gmail.com>
2020-03-29 22:27:24 +0200

    More compact teaser

--- src/components/hackathon.js
+++ src/components/hackathon.js
@@ -6,7 +6,7 @@ import './hackathon.scss';
 
 const Hackathon = ({id, date, title, text, start, end, total, location, locationUrl, isTeaser, children}) => (
   <div className={'hackathon ' + (isTeaser ? 'hackathon-teaser' : '')}>
-    <Container wide={isTeaser}>
+    <Container>
       <div className="hackathon-meta">
         {id}&ensp;{date && <>
           <span className="hackathon-date">{date}&ensp;<span className="nowrap">{start}-{end} ({total} h)</span></span>
