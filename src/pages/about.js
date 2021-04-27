5475a953e3bf32cd14fb8fb0e026a9665215bfe9
Carl Enlund <carl.enlund@gmail.com>
2021-04-27 20:22:37 +0200

    Smaller sizes

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,21 +8,14 @@ export default () => {
   return (
     <Layout>
       <Meta title="Outyard - About the hackathon" />
-      <Article title="What is Outyard anyway?">
+      <Article title="About">
         <p>
           Outyard is a hackathon at Uppsala University.
           The hackathon is meant to inspire personal projects and is a great way to learn programming.
         </p>
         <p>
-          Join us on Discord to get notified about future hackathons.
+          Join us on Discord and get notified about future hackathons.
           We recommend you have moderate programming experience and can for example draw things to the screen.
-        </p>
-        <p>
-          Outyard was started 2020 and is organized by Carl Enlund (<a href="mailto:carl@crl.io">carl@crl.io</a>).
-        </p>
-        <p>
-          The source code for this website is hosted on <a className="footer-link" href="http://github.com/outyard/outyard.github.io" target="_blank" rel="noreferrer">GitHub</a>.
-          Feel free to submit issues/pull requests!
         </p>
       </Article>
     </Layout>
